93b646b80a8bb9380d1c9a0da19299681fb38a6b
politutora <139717445+politutora@users.noreply.github.com>
2023-07-16 22:47:45 -0300

    primeiro commit

---
+++ database/migrations/2023_07_16_211737_create_produtos_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProdutosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+
+    {
+
+        Schema::create('produtos', function (Blueprint $table) {
+
+            $table->id();
+
+            $table->string('descricao');
+
+            $table->integer('qtd');
+
+            $table->float('precoUnitario');
+
+            $table->float('precoVenda');
+
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produtos');
+    }
+}
